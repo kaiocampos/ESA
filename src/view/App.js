c89af241de872a403a489169549b2c969a9c1322
Joao Marcos <joaomarc97@gmail.com>
2023-12-15 23:13:33 -0300

    iniciando o projeto

---
+++ src/view/App.js
@@ -0,0 +1,28 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       <h1>Educação Solidária Anônima</h1>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+      <body>
+        <p>
+          Olá
+        </p>
+      </body>
+      <fotter></fotter>
+
+    </div>
+  );
+}
+
+export default App;
